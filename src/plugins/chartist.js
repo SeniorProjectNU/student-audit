037da35ab67a122fcefe53e9ccad8cd52fa6c3d3
nurper <nurken_99@mail.ru>
2020-11-23 20:01:16 +0600

    comparison start

--- src/plugins/chartist.js
+++ src/plugins/chartist.js
@@ -1,4 +1,7 @@
 import Vue from 'vue'
 import 'chartist/dist/chartist.min.css'
+import 'chartist-plugin-legend'
+import 'chartist-plugin-axistitle'
+import 'chartist-plugin-tooltips'
 
 Vue.use(require('vue-chartist'))
